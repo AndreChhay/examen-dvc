d6a395b6a3c8bccef211402228e665d7eb512084
Ubuntu <ubuntu@ip-172-31-38-188.eu-west-1.compute.internal>
2025-10-24 12:54:05 +0000

    Update 5_evaluate.py: ajout to.csv predictions.csv + index FALSE

--- src/models/5_evaluate.py
+++ src/models/5_evaluate.py
@@ -9,7 +9,7 @@ X_test = pd.read_csv('./data/processed_data/X_test_scaled.csv')
 y_test = pd.read_csv('./data/processed_data/y_test.csv').values.ravel()
 
 predictions = gbr.predict(X_test)
-pd.DataFrame({"y_pred": predictions, "y_test": y_test}).to_csv("./data/processed_data/predictions.csv")
+pd.DataFrame({"y_pred": predictions, "y_test": y_test}).to_csv("./data/processed_data/predictions.csv", index=False)
 
 r2 = r2_score(y_test, predictions)
 mse = mean_squared_error(y_test, predictions)
